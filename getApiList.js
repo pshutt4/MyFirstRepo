55c418853ea761d225ad04552c19104e2e646d89
Paulina <pshutt@pointpark.edu>
2021-08-10 20:58:05 -0400

    changed the image trail in the Homepage.html page and made some changes to getApiLists.js

--- getApiList.js
+++ getApiList.js
@@ -1,3 +1,11 @@
+var apikey='2H6sVB7RCK3Ui5UiVS4w9R9jPrRuuRAl'
+var urlbase='https://api.nytimes.com/svc/search/v2/articlesearch.json?q=frogs&api-key=2H6sVB7RCK3Ui5UiVS4w9R9jPrRuuRAl'
+var keyword = 'Value from the textbox that the user inputs in here'
+var query = '?q='
+var apikeyidentity = 'api-key='
+
+//var url = urlbase + query + keyword + apikeyidentity + apikey;
+
 var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=frogs&api-key=2H6sVB7RCK3Ui5UiVS4w9R9jPrRuuRAl'
 
 function setup() {
